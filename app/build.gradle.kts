1d763ad30a06c37db4af91dc5d92372fdae4f3f7
deepak yadav <y2003deepak@gmail.com>
2026-06-07 15:39:59 +0530

    Use default debug signing configuration to fix build failure due to missing debug.keystore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,12 +28,6 @@ android {
       keyAlias = "upload"
       keyPassword = System.getenv("KEY_PASSWORD")
     }
-    create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
-      storePassword = "android"
-      keyAlias = "androiddebugkey"
-      keyPassword = "android"
-    }
   }
 
   buildTypes {
@@ -44,7 +38,7 @@ android {
       signingConfig = signingConfigs.getByName("release")
     }
     debug {
-      signingConfig = signingConfigs.getByName("debugConfig")
+      signingConfig = signingConfigs.getByName("debug")
     }
   }
   compileOptions {
